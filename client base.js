e4f7e49192c3c30e02e64c681d17bf494be9acb1
Josef Hamerson <126796405+pilarmoon03@users.noreply.github.com>
2023-03-02 17:52:33 +0200

    Create client base.js

---
+++ client base.js
@@ -0,0 +1,15 @@
+class client base {
+    constructor(settings) {
+        this.__settings = Object.assign(VideoPlayer.getDefaultSettings(), settings);
+        this.__interfase = null;
+        this.__buttons = null;
+        this.__wallpapers = null;
+        
+            }
+            
+return {
+            inter: '',
+            videoContainer: '.mplayer',
+            volume: 2,
+        };
+    }
